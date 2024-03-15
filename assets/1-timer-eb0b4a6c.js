a2d27716d253712715babd92cb6278d47315da97
OlenaPanf <148662268+OlenaPanf@users.noreply.github.com>
2024-03-15 23:07:42 +0000

    Deploying to gh-pages from @ OlenaPanf/goit-js-hw-10@abbd6a20023d96e0e004a45489a899bccbbfd34a 🚀

---
+++ assets/1-timer-eb0b4a6c.js
@@ -0,0 +1,2 @@
+import"./vendor-94c342ae.js";document.querySelector("[data-start]");function t(o){const n=Math.floor(o/864e5),c=Math.floor(o%864e5/36e5),e=Math.floor(o%864e5%36e5/6e4),s=Math.floor(o%864e5%36e5%6e4/1e3);return{days:n,hours:c,minutes:e,seconds:s}}console.log(t(2e3));console.log(t(14e4));console.log(t(2414e4));
+//# sourceMappingURL=1-timer-eb0b4a6c.js.map
